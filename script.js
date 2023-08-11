9817f806d6877385f6090e891d4bee3e7b8a53af
Michael <mickaellunzola@gmail.com>
2023-08-11 08:54:51 +0100

    add the variable in file script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-{ calculate } import './calculator';
+//{ calculate } import './calculator';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
+
+// initialisation de nos variable
+const acButton = document.getElementById('input');
+const resetButton = document.getElementById('reset');
+const clearButton = document.getElementById('clear');
+const equalsButton = document.getElementById('equals');
+const plusoumoinsButton = document.getElementById('plusoumoins');
+
+
+acButton.addEventListener("click", ()=> {
+    input.value = "";
+  });
+  
