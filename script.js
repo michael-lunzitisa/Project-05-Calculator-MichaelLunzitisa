f5ab0cbc491484011cf5187e540ab5ce17e62492
SmartDev901 <72765349+SmartDev901@users.noreply.github.com>
2023-08-11 16:07:04 +0000

    Ajout de la fonctionnalités dans form

--- script.js
+++ script.js
@@ -5,20 +5,38 @@
 // initialisation des variables
 const inputValue = document.getElementById('input');
 const acButton = document.getElementById('reset');
-const numTouch = document.querySelectorAll('.numpad');
 const clearButton = document.getElementById('clear');
+const numTouch = document.querySelectorAll('.numpad');
+
+// initialisation des variables des opération
+const moinButton = document.getElementById('minus');
+const addButton = document.getElementById('plus');
+const mulButton = document.getElementById('times');
+const divButton = document.getElementById('divideby');
 const equalsButton = document.getElementById('equals');
+const percentageButton = document.getElementById('percentage');
 const plusoumoinsButton = document.getElementById('plusoumoins');
 
+const form = document.querySelector('form');
 
+const calculButton = document.getElementById('calcul');
+// ecouteur d'evenement sur le form et on bloc la soumittion automatique du formulaire
+form.addEventListener('click', (e) => {
+  e.preventDefault();
+})
+
+// on parcour touts le button numérique
 for (let i = 0; i < numTouch.length; i++) {
-    numTouch[i].addEventListener('click', ()=>{
-        inputValue.value +=numTouch[i].innerText;
-    })
+  numTouch[i].addEventListener('click', () => {
+    inputValue.value += numTouch[i].innerText;
+    // on affiche le calcul au dessus de l'opération
+    calculButton.innerText += numTouch[i].textContent;
+  })
 }
 
+// E
+acButton.addEventListener('click', () => {
+  inputValue.value = '';
+  calculButton.innerText = '';
 
-acButton.addEventListener('click',()=>{
-     inputValue.value='';
-    
 })
