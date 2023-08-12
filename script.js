983960f2656218d9f7ad10e490583c0b8e834c3a
Michael Lunzola <Michael>
2023-08-13 00:32:48 +0100

    initialisation the variables git [200~it commit -minitialis

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 
-
+// initialisation des variables
+const input = document.getElementById('input');
+const form = document.querySelector('form');
+const resetClick = document.getElementById('reset');
+const clearClick = document.getElementById('clear');
+const buttons = document.querySelectorAll('.numpad');
+const equalClick = document.getElementById('equals');
+const toggleSign = document.getElementById('plusoumoins');
+const calculate = document.getElementById('calcul');
+const operatorsButtons = document.querySelectorAll('#plus, #times, #divideby, #minus, #percentage');
